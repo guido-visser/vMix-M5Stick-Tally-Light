f8f50b5f6409626c7bd76233a03ce18cc7462aa3
Guido Visser <guido.vsr@gmail.com>
2021-07-03 09:28:03 +0200

    Patch version bump

--- src/a_GLOBAL/a_GLOBAL.ino
+++ src/a_GLOBAL/a_GLOBAL.ino
@@ -29,4 +29,4 @@ int CONN_INT = 0;
 int MODE = 0; //0 for words like SAFE, PRE and LIVE. 1 for numbers with changing background
 int JUSTLIVE = 0; //When 1, SAFE and PRE are not used. Just the LIVE screen
 
-String semver = "2.4.0";
+String semver = "2.4.1";
